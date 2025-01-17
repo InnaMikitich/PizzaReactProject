b29a1a9c6aade8378e24fee46f4a51aef1e1ee99
InnaMikitich <i.seergee@gmail.com>
2025-01-17 16:22:06 +0300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import {Header, Categories} from './Components';
 
-import Header from './Components/Header';
-import Button from './Components/Button';
+
 
 function App() {
-
 
   return (
     <div className="wrapper">
@@ -12,16 +11,15 @@ function App() {
     <div className="content">
       <div className="container">
         <div className="content__top">
-          <div className="categories">
-            <ul>
-              <li className="active">Все</li>
-              <li>Мясные</li>
-              <li>Вегетарианская</li>
-              <li>Гриль</li>
-              <li>Острые</li>
-              <li>Закрытые</li>
-            </ul>
-          </div>
+        <Categories
+        onClick={(name) => console.log(name)}
+        items={[
+          'Мясные',
+          'Вегетарианская',
+          'Гриль',
+          'Острые',
+          'Закрытые'
+        ]}/>
           <div className="sort">
             <div className="sort__label">
               <svg
