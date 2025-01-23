fb10b27eca2f73c24ceb98345c2e9c46d71fde07
InnaMikitich <i.seergee@gmail.com>
2025-01-23 16:02:08 +0300

    save changes in component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import {Header} from './Components';
 import {Home, Cart} from './pages';
+import { Route, Routes } from 'react-router-dom';
 
 
 
 
 function App() {
+React.useEffect(() => {
 
+}, []);
   return (
     <div className="wrapper">
    <Header />
     <div className="content">
-    <Home />
+    <Routes>
+    <Route path='/' element={<Home/>} exact />
+    <Route path='/cart' element={<Cart/>} exact />
+    </Routes>
     </div>
   </div>
   );
