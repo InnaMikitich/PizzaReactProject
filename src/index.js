555db3dbddb4435f76e945cd1b06ece4a7db10a2
InnaMikitich <i.seergee@gmail.com>
2025-02-03 15:03:33 +0300

    create reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from "react-redux";
 import store, { incremented } from './redux/store';
 
 
@@ -13,18 +14,16 @@ import App from './App';
 
 
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
     
-    
-  
-  </React.StrictMode>
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,8 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
+import rootReducer from './reducers/'
 
-const counter= createSlice({
-    name: 'counter',
-    initialState: {
-      value: 0
-    },
-    reducers: {
-      incremented: state => {
-        state.value += 1
-      },
-      decremented: state => {
-        state.value -= 1
-      }, 
-      //setValue: (state, action) => {
-        //state.value = action.payload
-     // }
-    }
-  })
+const store = configureStore({
+  reducer: rootReducer
+});
 
-export const { incremented, decremented, setValue } = counter.actions;
-  const store = configureStore(
-   {reducer: counter.reducer}
-  )
- 
-  export default store;
+export default store;
