0542cff85e2a8b2b7b2a79c5d8d0006325cea030
InnaMikitich <i.seergee@gmail.com>
2025-02-07 15:08:13 +0300

    rewrote the class component with hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
 import React from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 import {Header} from './Components';
 import {Home, Cart} from './pages';
 import { Route, Routes } from 'react-router-dom';
-import { setPizzas, setPizzas as setPizzasAction } from './redux/action/pizzas';
+import { setPizzas } from './redux/action/pizzas';
 
 
 
 
-//function App() {
-
-
-//React.useEffect(() => {
-//axios.get('http://localhost:3000/db.json').then(({ data })=> {
-  //setPizzas(data.pizzas);
-////});
-
-//}, []);
-  
-//}
-
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data })=> {
-     this.props.setPizzas(data.pizzas);
+function App( ) {
+  const dispatch = useDispatch();
+  const storage = useSelector(({pizzas, filters}) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy
+    };
   });
-}
+  console.log(storage)
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data })=> {
+      dispatch(setPizzas(data.pizzas));
+  });
+}, []);
 
-  render() {
-    return (
-      <div className="wrapper">
-     <Header />
-      <div className="content">
-      <Routes>
-      <Route path='/' element={<Home items={this.props.items} />} exact />
-      <Route path='/cart' element={<Cart/>} exact />
-      </Routes>
-      </div>
+  return (
+    <div className="wrapper">
+   <Header />
+    <div className="content">
+    <Routes>
+    <Route path='/' element={<Home items={[]} />} exact />
+    <Route path='/cart' element={<Cart/>} exact />
+    </Routes>
     </div>
-    );
-  }
+  </div>
+  );
+
 }
 
+
+export default App;
+
+/*
+return {
+  items: state.pizzas.items,
+  filters: state.filters
+}
+
+/*
 const mapStateToProps = state => {
   return {
     items: state.pizzas.items,
+    filters: state.filters
   }
 }
 
@@ -57,3 +63,5 @@ const mapDispatchToProps = dispatch => {
   };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+*/
+
