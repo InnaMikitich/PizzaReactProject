1b40c3bfbd2b61c7d1cf1c8b78c8d8c61fe6a625
Inna <i.seergee@gmail.com>
2025-06-06 13:13:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from "react-redux";
-import store, { incremented } from './redux/store';
+import store from './redux/store';
 
 
 
