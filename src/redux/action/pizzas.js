993d401d5e4919b441af69cf40c24827a6d7c99a
Inna <i.seergee@gmail.com>
2025-06-09 15:56:44 +0300

    Update pizzas.js
    fix

--- src/redux/action/pizzas.js
+++ src/redux/action/pizzas.js
@@ -1,4 +1,4 @@
-/*import axios from 'axios';
+import axios from 'axios';
 
 export const setLoaded = (payload) => ({
 type: 'SET_LOADED',
@@ -13,8 +13,8 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
         }&_sort=${sortBy}&_order=desc`).then(({ data }) => {
          dispatch(setPizzas(data));
       });
-}; 
-/*
+};  */
+
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false));
     
@@ -38,40 +38,17 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
 export const setPizzas = (items) => ({
     type: 'SET_PIZZAS',
     payload: items,
-}); */
-
-import axios from 'axios';
-
-export const setLoaded = (payload) => ({
-  type: 'SET_LOADED',
-  payload
-});
+}); 
 
 
-export const fetchPizzas = (sortBy, category) => (dispatch) => {
-  dispatch(setLoaded(false));
 
-
+/*
   const baseUrl = 'http://192.168.31.211:3001'; 
 
   const url = `${baseUrl}/pizzas?${
     category !== null ? `category=${category}&` : ''
   }_sort=${sortBy.type}&_order=${sortBy.order}`;
 
-  console.log("Запрос к API:", url);
+  console.log("Запрос к API:", url); */
 
-  axios.get(url)
-    .then(({ data }) => {
-      dispatch(setPizzas(data));
-    })
-    .catch((error) => {
-      console.error("Ошибка при загрузке пицц:", error);
-      alert("Произошла ошибка при загрузке данных. Пожалуйста, попробуйте позже.");
-    });
-};
-
-export const setPizzas = (items) => ({
-  type: 'SET_PIZZAS',
-  payload: items,
-});
 
